376ce348f09c2b39c54ec3ca8de16bbef9ddb414
Sam Mussell <sam@streetsaheadllc.com>
2012-07-07 18:02:44 -0600

    refactor a little and add liscence

--- people/AppDelegate.m
+++ people/AppDelegate.m
@@ -1,3 +1,24 @@
+//
+// Copyright (c) 2012 Streets Ahead LLC.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal 
+// in the Software without restriction, including without limitation the rights to use, 
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+// Software, and to permit persons to whom the Software is furnished to do so, subject 
+// to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all 
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR 
+// A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
 #import "AppDelegate.h"
 #import "Person.h"
 #import "NSDate+Extra.h"
@@ -41,7 +62,16 @@
                             bday:[NSDate dateWithMonth:2 day:8 year:1984] title:@"Salesman" 
                          twitter:@""
                            photo:@"http://sammussell.com/jim.jpg"];
+
+    for (int i = 0; i < 100; i++) {
+        [Person createPersonWithName:@"Terry Keeney" 
+                           startdate:[NSDate dateWithMonth:5 day:30 year:2007] 
+                                bday:[NSDate dateWithMonth:3 day:9 year:1985] title:@"Ambassador of Awesome" 
+                             twitter:@"tkeeney2"
+                               photo:@"http://sammussell.com/terry.png"];
+    }
     
+
     [self saveContext];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateCanTweet) name:ACAccountStoreDidChangeNotification object:nil];
--- people/SlidyTableCell.m
+++ people/SlidyTableCell.m
@@ -1,3 +1,24 @@
+//
+// Copyright (c) 2012 Streets Ahead LLC.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal 
+// in the Software without restriction, including without limitation the rights to use, 
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+// Software, and to permit persons to whom the Software is furnished to do so, subject 
+// to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all 
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR 
+// A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
 #import "SlidyTableCell.h"
 #import "PersonView.h"
 #import "Person.h"
@@ -5,19 +26,18 @@
 #import <Twitter/Twitter.h>
 
 @interface SlidyTableCell()
-@property (assign) CGPoint touchStart;
-@property (assign) NSTimeInterval startTime;
-
 - (void) animateToX:(CGFloat)x y:(CGFloat)y withDuration:(CGFloat)duration;
 @end
 
-@implementation SlidyTableCell
+@implementation SlidyTableCell {
+    CGPoint _touchStart;
+    NSTimeInterval _startTime;
+    UIView* _optionView;
+}
 
 @synthesize actionDelegate = _actionDelegate;
 @synthesize personView = _personView;
-@synthesize touchStart = _touchStart;
 @synthesize actionCell = _actionCell;
-@synthesize startTime = _startTime;
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier person:(Person*)person {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
@@ -48,8 +68,8 @@
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     NSLog(@"touches began");
     [super touchesBegan:touches withEvent:event];
-    self.touchStart = [[touches anyObject] locationInView:self.personView];
-    self.startTime = [[NSDate date] timeIntervalSince1970] * 1000;
+    _touchStart = [[touches anyObject] locationInView:self.personView];
+    _startTime = [[NSDate date] timeIntervalSince1970] * 1000;
 }
 
 - (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -80,21 +100,21 @@
     ((UITableView*)self.superview).scrollEnabled = YES;
     
     CGPoint current = [[touches anyObject] locationInView:self];
-    CGFloat delta = current.x - self.touchStart.x;
-    NSLog(@"touchstart %f, %f", self.touchStart.x, current.x);
+    CGFloat delta = current.x - _touchStart.x;
+    NSLog(@"touchstart %f, %f", _touchStart.x, current.x);
     CGFloat xpoint = self.personView.center.x + delta;
     NSLog(@"delta %f", delta);
     
     NSTimeInterval endTime = [[NSDate date] timeIntervalSince1970] * 1000;
-    NSTimeInterval deltaTime = endTime - self.startTime;
+    NSTimeInterval deltaTime = endTime - _startTime;
     NSLog(@"delta time %f", deltaTime);
     
     if(!self.actionCell) {
-        if(xpoint > 500 || (delta > 80 && deltaTime < 100)) {
+        if(xpoint > 500 || (delta > 80 && deltaTime < 200)) {
             [self.actionDelegate becomeCurrentActionCell:self];
             [self animateToX:280 y:0 withDuration:.1];
             self.actionCell = YES;
-        } else if(xpoint < -100 || (delta < -80 && deltaTime < 100)) {
+        } else if(xpoint < -100 || (delta < -80 && deltaTime < 200)) {
             [self.actionDelegate becomeCurrentActionCell:self];
             [self animateToX:-280 y:0 withDuration:.1];
             self.actionCell = YES;
@@ -117,14 +137,14 @@
     CGPoint current = [[touches anyObject] locationInView:self.personView];
     
     CGPoint totalCurrent = [[touches anyObject] locationInView:self];
-    CGFloat deltax = (totalCurrent.x - self.touchStart.x);
+    CGFloat deltax = (totalCurrent.x - _touchStart.x);
     NSLog(@"delta %f", deltax);
-    if(abs(deltax) > 3) {
+    if(abs(deltax) > 10) {
         NSLog(@"SCROLL DISABLE");
         ((UITableView*)self.superview).scrollEnabled = NO;
         self.personView.backgroundColor = [UIColor colorWithRed:0.087 green:0.087 blue:0.087 alpha:1.000];
         
-        self.personView.center = CGPointMake( self.personView.center.x + (current.x - self.touchStart.x), 
+        self.personView.center = CGPointMake( self.personView.center.x + (current.x - _touchStart.x), 
                                              self.personView.center.y);   
     }
     
